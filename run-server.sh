ee5075c9214361b9166de1c063553ad14ecc1d4c
Temur <tim_1998@bk.ru>
2019-03-18 13:34:04 +0300

    style[scripts]: remove irrelevant comments
    #style

--- run-server.sh
+++ run-server.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-#docker-compose -f docker_config/compose-db.yml build
-#docker stack deploy --compose-file docker_config/compose-db.yml swp
-
-docker-compose -p swp -f docker_config/compose-db.yml up -d #--build #--force-recreate
+docker-compose -p swp -f docker_config/compose-db.yml up -d
 docker-compose -p swp -f docker_config/compose-web.yml up -d --build --force-recreate
-#echo "Wait"
-#sleep 10
 
 echo "Completed"
 
