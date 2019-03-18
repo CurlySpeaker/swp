45e600028520cc2143713f0fd88ba3134dc7fa8c
Rishat Maksudov <mcwchitos@mail.ru>
2019-03-18 15:33:28 +0300

    feat[front]: add a base app and basic html files
    HTML files: dashboard, authorization, account.
    
    #html #front

---
+++ baseApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('authorization/', views.auth, name='auth'),
+    path('account/', views.acc, name='acc'),
+]
---
+++ baseApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'baseApp/dashboard.html')
+
+
+def auth(request):
+    return render(request, 'baseApp/authorization.html')
+
+
+def acc(request):
+    return render(request, 'baseApp/account.html')
