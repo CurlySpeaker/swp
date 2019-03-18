99b44387be2333773451ba6b9c0669c09cc8a28c
Temur <tim_1998@bk.ru>
2019-03-18 16:58:38 +0300

    temp[script]: add sudo to docker-compose commands
    #temp #script

--- run-server.sh
+++ run-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose -p swp -f docker_config/compose-db.yml up -d
-docker-compose -p swp -f docker_config/compose-web.yml up -d --build --force-recreate
+sudo docker-compose -p swp -f docker_config/compose-db.yml up -d
+sudo docker-compose -p swp -f docker_config/compose-web.yml up -d --build --force-recreate
 
 echo "Completed"
 
