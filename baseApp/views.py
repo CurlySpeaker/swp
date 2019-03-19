fe093b8bc5fcfea71a8ef933944eb893572a0075
Susan Green <35573111+SueGreen@users.noreply.github.com>
2019-03-19 03:05:22 +0300

    Update views.py
    Update views.py to make views.py give some data to templates and render it - just to test that it works

--- baseApp/views.py
+++ baseApp/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'baseApp/dashboard.html')
+    p1 = {"name": "name1",
+          "time_spent": "1",
+          "descr": "awesome project"}
+    p2 = {"name": "name2",
+          "time_spent": "2",
+          "descr": "great project"}
+    projects = [p1, p2]
+    context = {
+        "projects": projects
+    }
+
+    return render(request, 'baseApp/project.html', context=context)
 
 
 def auth(request):
@@ -12,5 +23,6 @@ def auth(request):
 def acc(request):
     return render(request, 'baseApp/account.html')
 
+
 def registration(request):
-	return render(request, 'baseApp/registration.html')
+    return render(request, 'baseApp/registration.html')
